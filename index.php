f4e183caf0928523ca102351b962edb6ef2bbf58
Gregory Carter Boyarko <boyarkog@bentley.cs.colostate.edu>
2016-04-28 16:33:15 -0600

    skeleton files

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+include 'inc/header.php';
+
+?>
+
+<div class="container-fluid">
+    <h1>My First Bootstrap Page</h1>
+    <p>This is some text.</p>
+</div>
+
+<div class="row">
+    <div class="col-sm-4">.col-sm-4</div>
+    <div class="col-sm-8">.col-sm-8</div>
+</div>
+
+<?php
+
+include 'inc/footer.php';
+
+?>
