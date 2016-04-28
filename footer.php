1ab82cf61c3ad290079045ffc56f3d8694ec67df
Gregory Carter Boyarko <boyarkog@bentley.cs.colostate.edu>
2016-04-28 17:42:05 -0600

    switching to other codebase

--- footer.php
+++ footer.php
@@ -1,4 +1,13 @@
-<?php
+<!-- Start of included footer -->
+<footer>
+    <div id="mainFooter" style="width:100;">
+	<a id="bottomButton" href="adopt.php">ADOPT A DOG TODAY!</a>
+    </div>
+    <div id="copyrightBar">
+        <p>This site is part of a CSU <a href="http://www.cs.colostate.edu/~ct310">CT 310</a> Course Project.</p>
+    </div>
+</footer>
 
-?>
+</body>
 </html>
+
