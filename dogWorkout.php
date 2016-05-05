feebf0ceebf00ee74862c49cb95bb1bdc50670c8
Liyan <liyanxu94@gmail.com>
2016-05-04 18:19:43 -0600

    tabs

--- dogWorkout.php
+++ dogWorkout.php
@@ -58,13 +58,17 @@ include 'top.php';
 	}
 	?>
 -->
-	<div class="digInfoTabs">
+<div class="digInfoTabs">
 		<ul>
 				<li><a href= <?php echo "dogContact.php?pet_id=".$petID ?> >Contact Info</a></li>
 				<li><a href= <?php echo "dogMessages.php?pet_id=".$petID ?> >Messages</a></li>
-				<li><a href= <?php echo "dogWorkout.php?pet_id=".$petID ?> >Workout</a></li>
+				<li><a href= <?php echo "dogWorkout.php?pet_id=".$petID ?> style="background-color:orange">Workout</a></li>
 				<li><a href= <?php echo "dogMedical.php?pet_id=".$petID ?> >Medical History</a></li>
 		</ul>
+	</div>
+	
+	<div>
+		<p>Workout: 10am - 5pm MWF</p>
 	</div>
 	</div>
 	
