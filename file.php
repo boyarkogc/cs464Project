9d5b02c4dff43211fadbde5c84dac48a4b313bfb
Xinghui Fei <xinghui@bentley.cs.colostate.edu>
2016-05-05 21:02:54 -0600

    aaa

---
+++ file.php
@@ -0,0 +1,18 @@
+<?php
+$pageTitle = "Client";
+include 'control.php'; 
+include 'top.php'; 
+?>
+</head>
+<body>
+	<?php include 'header.php'; ?>
+	<div id="content">
+<h2>My File</h2>
+<ul>
+<li><a href="./images/file1.d" download="Client Waiver Form">Client Waiver Form</a></li>
+<li><a href="./images/file2.doc" download="Allergen Form">Allergen Form</a></li>
+<li><a href="./images/file3.doc" download="Disability Form">Disability Form</a></li>
+</ul>
+</div>
+</body>
+<?php include 'footer.php'; ?>
