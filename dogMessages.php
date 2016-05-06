ab97350ff758842ecc26c5a7022c76f8e9a03a5b
Tyler Monteferrante <montetm@jaguar.cs.colostate.edu>
2016-05-05 19:04:02 -0600

    p

--- dogMessages.php
+++ dogMessages.php
@@ -63,10 +63,10 @@ include 'top.php';
 -->
 <div class="digInfoTabs">
 		<ul>
-			<li><a href= <?php echo "dogContact.php?pet_id=".$petID ?> >Contact Info</a></li>
-			<li><a href= <?php echo "dogMessages.php?pet_id=".$petID ?> style="background-color:orange">Messages</a></li>
-			<li><a href= <?php echo "dogWorkout.php?pet_id=".$petID ?> >Workout</a></li>
-			<li><a href= <?php echo "dogMedical.php?pet_id=".$petID ?> >Medical History</a></li>
+				<li><a href= <?php echo "dogContact.php?pet_id=".$petID ?> >Contact Info</a></li>
+				<li><a href= <?php echo "dogMessages.php?pet_id=".$petID ?> style="background-color:orange">Messages</a></li>
+				<li><a href= <?php echo "dogWorkout.php?pet_id=".$petID ?> >Workout</a></li>
+				<li><a href= <?php echo "dogMedical.php?pet_id=".$petID ?> >Medical History</a></li>
 		</ul>
 	</div>
 	
